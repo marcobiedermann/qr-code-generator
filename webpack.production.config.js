0c4f4f4731293bc769d1047e0b6ca3daa7ade550
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-18 23:54:15 +0200

    add webpack

---
+++ webpack.production.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: [
+      path.resolve(__dirname, 'client/assets/js/index'),
+    ],
+  },
+  output: {
+    filename: 'assets/js/[name].js',
+    path: path.resolve(__dirname, 'server/public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.js',
+    ],
+  },
+};
