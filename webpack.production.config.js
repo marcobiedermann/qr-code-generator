974a6d3f534b90126c0300829a49392e87621217
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-20 20:21:48 +0200

    add css-loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -24,6 +24,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
