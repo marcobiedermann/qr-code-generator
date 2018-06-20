628b294852f8267310f82c2ab713331967e94848
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-20 23:32:42 +0200

    add Redux DevTools Extension

--- client/assets/js/store/index.js
+++ client/assets/js/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import middleware from '../middleware';
 import reducers from '../reducers';
 
-const store = createStore(reducers, middleware);
+const store = createStore(
+  reducers,
+  composeWithDevTools(middleware),
+);
 
 export default store;
