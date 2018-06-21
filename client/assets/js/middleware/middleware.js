c2187d2bc77641b2d3ec3c1424ec4224026e134b
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-21 22:02:18 +0200

    remove unused code

--- client/assets/js/middleware/middleware.js
+++
@@ -1,7 +0,0 @@
-import { routerMiddleware } from 'connected-react-router'
-import { applyMiddleware } from 'redux';
-import history from '../router/history';
-
-const middleware = applyMiddleware(routerMiddleware(history));
-
-export default middleware;
