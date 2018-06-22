ebcc0aecbd3d5498731e5a555036c0b47f4257b4
Marco Biedermann <marco.biedermann@gmx.de>
2018-06-23 00:57:06 +0200

    add tabs

--- client/assets/js/pages/Index/index.jsx
+++ client/assets/js/pages/Index/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import {
+  Tab,
+  Tabs,
+} from '@material-ui/core';
+import React, { Component } from 'react';
 
-const IndexPage = () => (
-  <div>
-    Hello, world!
-  </div>
-);
+class IndexPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 0,
+    };
+  }
+
+  handleChange(event, value) {
+    this.setState({
+      value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Tabs value={this.state.value} onChange={(event, value) => this.handleChange(event, value)}>
+          <Tab label="Text" href="#text" />
+          <Tab label="URL" href="#url" />
+          <Tab label="Contact" href="#contact" />
+          <Tab label="Phone" href="#phone" />
+          <Tab label="SMS" href="#sms" />
+          <Tab label="WiFi" href="#wifi" />
+
+        </Tabs>
+        {this.state.value === 0 && <div>Text</div>}
+        {this.state.value === 1 && <div>URL</div>}
+        {this.state.value === 2 && <div>Contact</div>}
+        {this.state.value === 3 && <div>Phone</div>}
+        {this.state.value === 4 && <div>SMS</div>}
+        {this.state.value === 5 && <div>WiFi</div>}
+      </div>
+    );
+  }
+}
 
 export default IndexPage;
