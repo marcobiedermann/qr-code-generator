20821e6225253f53d122ca79e657c9d0bf74e95a
Marco Biedermann <marco.biedermann@gmx.de>
2020-09-25 16:08:36 +0200

    feat: Redux
    Remove Redux

--- client/assets/js/index.jsx
+++ client/assets/js/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import { render } from "react-dom";
 import 'typeface-roboto/index.css';
-import Router from './router';
-import store from './store';
+import Layout from "./components/Layout";
+import IndexPage from "./pages/Index";
 
 const Root = () => (
-  <Provider store={store}>
-    <Router />
-  </Provider>
+  <Layout>
+    <IndexPage />
+  </Layout>
 );
 
 render(<Root />, document.getElementById('root'));
--- client/assets/js/store/middleware.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware } from 'redux';
-import { routerMiddleware } from 'connected-react-router';
-import history from '../router/history';
-
-const middleware = applyMiddleware(routerMiddleware(history));
-
-export default middleware;
